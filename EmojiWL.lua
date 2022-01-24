7d8ceef12975fd99f559409a4764d5fed615d367
Stefxtjuh <44397567+Stefxtjuh@users.noreply.github.com>
2022-01-24 16:50:44 +0100

    Update EmojiWL.lua

--- EmojiWL.lua
+++ EmojiWL.lua
@@ -1,55 +1,17 @@
-local owner = {
-  350167333,
-}
-
-local admin = {
+local OwnerId = {
+	350167333,
 
 }
-local mod = {
 
-}
-local prem = {
-  
-}
-
-local formatNumber = (function(n)
-    n = tostring(n)
-    return n:reverse():gsub("%d%d%d", "%1,"):reverse():gsub("^,", "")
-end)
-
-local function CheckEmoji()
-    for i,v in pairs(game:GetService('Workspace').Players:GetChildren()) do
-        if v:FindFirstChild('UpperTorso') then
-            if not v:FindFirstChild('UpperTorso'):FindFirstChild('OriginalSize') then
-                local plrcheck = game:GetService('Players'):FindFirstChild(v.Name)
-                local plrcheck = game.Players:FindFirstChild(v.Name)
-                if plrcheck then
-                    local plrID = game.Players:FindFirstChild(v.Name).UserId
-          
-                     elseif table.find(owner, plrID) then
-                        if v:FindFirstChildWhichIsA('Humanoid') then
-                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑]' .. game.Players[v.Name].DisplayName)
-                        end
-          
-                    elseif table.find(admin, plrID) then
-                        if v:FindFirstChildWhichIsA('Humanoid') then
-                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🔨]' .. game.Players[v.Name].DisplayName)
-                        end
-          
-                    elseif table.find(mod, plrID) then
-                        if v:FindFirstChildWhichIsA('Humanoid') then
-                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⚙]' .. game.Players[v.Name].DisplayName)
-                        end
-           
-                    elseif table.find(prem, plrID) then
-                        if v:FindFirstChildWhichIsA('Humanoid') then
-                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]' .. game.Players[v.Name].DisplayName)
-                        end
-                    end
-                end
-            end
-        end
-    end
+local function main()
+	for i,v in pairs(game.Players:GetChildren()) do
+		if table.find(vipIDs,v.UserId) then
+			if v.Character then
+				v.Character:FindFirstChild("Humanoid").DisplayName = "[👑]"..v.DisplayName
+			end
+		end
+	end
 end
+local success,err = pcall(main)
 
-local succ, errr = pcall(CheckEmoji)
+return OwnerId
