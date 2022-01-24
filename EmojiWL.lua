a881f3514217863f6ad41935e42a4ca880d6732e
Stefxtjuh <44397567+Stefxtjuh@users.noreply.github.com>
2022-01-24 16:38:53 +0100

    Create EmojiWL.lua

---
+++ EmojiWL.lua
@@ -0,0 +1,55 @@
+local owner = {
+  350167333
+}
+
+local admin = {
+
+}
+local mod = {
+
+}
+local prem = {
+  
+}
+
+local formatNumber = (function(n)
+    n = tostring(n)
+    return n:reverse():gsub("%d%d%d", "%1,"):reverse():gsub("^,", "")
+end)
+
+local function checkswag()
+    for i,v in pairs(game:GetService('Workspace').Players:GetChildren()) do
+        if v:FindFirstChild('UpperTorso') then
+            if not v:FindFirstChild('UpperTorso'):FindFirstChild('OriginalSize') then
+                local plrcheck = game:GetService('Players'):FindFirstChild(v.Name)
+                local plrcheck = game.Players:FindFirstChild(v.Name)
+                if plrcheck then
+                    local plrID = game.Players:FindFirstChild(v.Name).UserId
+          
+                     elseif table.find(owner, plrID) then
+                        if v:FindFirstChildWhichIsA('Humanoid') then
+                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑]' .. game.Players[v.Name].DisplayName)
+                        end
+          
+                    elseif table.find(admin, plrID) then
+                        if v:FindFirstChildWhichIsA('Humanoid') then
+                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🔨]' .. game.Players[v.Name].DisplayName)
+                        end
+          
+                    elseif table.find(mod, plrID) then
+                        if v:FindFirstChildWhichIsA('Humanoid') then
+                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⚙]' .. game.Players[v.Name].DisplayName)
+                        end
+           
+                    elseif table.find(prem, plrID) then
+                        if v:FindFirstChildWhichIsA('Humanoid') then
+                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]' .. game.Players[v.Name].DisplayName)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+local succ, errr = pcall(checkswag)
