374c64a6f12a5b3790588de84d213534b5055b36
Stefxtjuh <44397567+Stefxtjuh@users.noreply.github.com>
2022-02-03 17:11:13 +0100

    Update prem.lua

--- prem.lua
+++ prem.lua
@@ -1,44 +1,27 @@
 local PremStarData = {
- [350167333] = true,-- hecate
+ 350167333,
 }
 
---[[
+
 local PremStarLegendData = {
-[587438679] = true, --jaroah
+ 587438679,
+}
+local OwnerAndMods = {
+ 350167333,
 }
 
-local function loademoji()
-    for i,v in pairs(game:GetService('Workspace').Players:GetChildren()) do
-        if v:FindFirstChild('UpperTorso') then
-            if not v:FindFirstChild('UpperTorso'):FindFirstChild('OriginalSize') then
-                local plrcheck = game:GetService('Players'):FindFirstChild(v.Name)
-                if plrcheck then
-                    local plrID = game:GetService('Players'):FindFirstChild(v.Name).UserId
-                    if plrID == 350167333 then --stef (owner)
-                        if v:FindFirstChildWhichIsA('Humanoid') then           
-                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🪐]' .. game.Players[v.Name].DisplayName) 
-                        end
-                    elseif plrID == 2834470639 then
-                        if v:FindFirstChildWhichIsA('Humanoid') then           
-                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🌈+🏳‍🌈+💅]'.. game.Players[v.Name].DisplayName) 
-                        end
-            
-                       elseif table.find(PremStarLegendData, plrID) then
-                        if v:FindFirstChildWhichIsA('Humanoid') then
-                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑]' .. game.Players[v.Name].DisplayName)
-                        end
-            
-                    elseif table.find(premdata, plrID) then
-                        if v:FindFirstChildWhichIsA('Humanoid') then
-                            v:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐]' .. game.Players[v.Name].DisplayName)
-                        end
-                    end
-                end
-            end
-        end
-    end
+
+
+
+local function main()
+	for i,v in pairs(game.Players:GetChildren()) do
+		if table.find(PremStarData,v.UserId) then
+			if v.Character then
+				v.Character:FindFirstChild("Humanoid").DisplayName = "[⭐]"..v.DisplayName
+			end
+		end
+	end
 end
 
-local succ, errr = pcall(loademoji)]] 
+local success,err = pcall(main)
  
- return PremStarData;
